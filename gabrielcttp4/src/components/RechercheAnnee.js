2f0cc448ca91873785d211046e8481e2ca226274
GCTWOrLD <gabriel.caron.tawil@gmail.com>
2022-05-05 22:38:33 -0400

    React Recherche par Année fonctionel

--- gabrielcttp4/src/components/RechercheAnnee.js
+++ gabrielcttp4/src/components/RechercheAnnee.js
@@ -1,16 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-const RechercheAnnee = () => (
-    <div>
-        <h3>Recherche par année</h3>
-        <form>
-            <label>
-                Année:
-                <input type="text" name="annee" />
+const RechercheAnnee = () => {
+    const [data, setData] = useState(null);
+    const [query, setQuery] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!query) {
+            alert('Veuillez entrer une année')
+            return
+        }
+    
+        async function fetchData() {
+          const response = await fetch(`http://localhost:8080/documents/annee/${query}`);
+          const data = await response.json();
+          setData(data);
+        }
+        fetchData();
+    };
+
+      return (
+        <div>
+            <h3>Recherche par auteur</h3>
+          <form onSubmit={handleSubmit}>
+            <label>Année:
+                <input
+                type="text"
+                placeholder="Année"
+                value={query}
+                onChange={(e) => {
+                  setQuery(e.target.value);
+                }}
+              />
             </label>
-            <input type="submit" value="Submit" />
-        </form>
-    </div>
-)
+            <input type="submit" value="Submit"/>
+          </form>
+          <h3>Titre / Auteur / Année / Catégorie / Examplaires</h3>
+    
+          {data &&
+            data.map((document) => (
+              <div key={document.id}>
+                <p>{document.titre} / {document.auteur}</p>
+              </div>
+            ))}
+        </div>
+      );
+}
 
 export default RechercheAnnee;
